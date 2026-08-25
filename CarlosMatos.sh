9ad6088c1669c123ce56ff8896551ab86e6e1e8c
Carlos <cmatos3@ggc.edu>
2026-08-25 17:17:51 -0400

    Added CarlosMatos Script for assn5a

---
+++ CarlosMatos.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hello! Carlos Matos"
+echo "Where do go to school?"
+read school
+echo "I see you go to $school";
+
+echo "What your concentration?"
+read concentration
+echo "Pretty cool, you are an $concentration looks like a cool path to follow."
